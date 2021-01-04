750458b876be27e0c0ce8cffbff9097b26925215
sdelane2 <seanmartindelaney@gmail.com>
2021-01-04 14:03:23 -0500

    button styling

--- star-bar-frontend/src/Components/Login.js
+++ star-bar-frontend/src/Components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/Login.css'
 
 class Login extends React.Component{
     
@@ -23,11 +24,19 @@ class Login extends React.Component{
     render(){
         // console.log(this.state)
         return(
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="username" value={this.state.username} onChange={this.handleChange} name="username"/>
-                <input type="text" placeholder="password" value={this.state.password} onChange={this.handleChange} name="password"/>
-                <button>Log In</button>
-            </form>
+            <main id="login">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="username" value={this.state.username} onChange={this.handleChange} name="username"/>
+                    <input type="text" placeholder="password" value={this.state.password} onChange={this.handleChange} name="password"/>
+                    <button
+                    style={{
+                        width: '100%',
+                        boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)'
+                      }}>
+                    Log In
+                    </button>
+                </form>
+            </main>
 
         )
     }
